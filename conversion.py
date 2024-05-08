a1da86488d0fbe1ca5d6b2fe12666b55014ab6d4
Beatriz Leite <beatriz.leite@healthcatalyst.com>
2024-05-08 17:32:13 -0600

    initial commit

---
+++ conversion.py
@@ -0,0 +1,48 @@
+##USD to CAD converter
+#This conversion rate is based on 5/8/2024
+
+print("USD to CAD converter")
+print("\n")
+
+usd = 50
+cad = 111
+
+usd_to_cad = usd * 1.37
+cad_to_usd = cad / 1.37
+
+print(usd, "usd is", usd_to_cad, "cad")
+print(cad, "cad is", cad_to_usd, "usd")
+print("\n\n\n")
+
+
+##Celsius to Farenheit converter
+
+print("Celsius to Farenheit converter")
+print("\n")
+
+cel = 24
+far = 54
+
+cel_to_far = (9/5 * cel) + 32
+far_to_cel = (far - 32) * 5/9
+
+print(cel, "°C is", cel_to_far, "°F")
+print(far, "°C is", far_to_cel, "°C")
+print("\n\n\n")
+
+
+##Miles to Kilometers converter
+
+print("Miles to Kilometers converter")
+print("\n")
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+
+print(miles, "miles is", round(miles_to_kilometers, 2), "kilometers")
+print(kilometers, "kilometers is", round(kilometers_to_miles, 2), "miles")
+
+print("\n\n\n")
